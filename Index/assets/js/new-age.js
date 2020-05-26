ddfbdaa6a5268e7e3a2307190f7a6d52bf3ebc1c
Irfan Backer <irfuvk@gmail.com>
2020-05-27 00:42:09 +0530

    New main UI

--- Index/assets/js/new-age.js
+++ Index/assets/js/new-age.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top)
+          scrollTop: (target.offset().top - 48)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -22,7 +22,21 @@
 
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
-    target: '#sideNav'
+    target: '#mainNav',
+    offset: 54
   });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
